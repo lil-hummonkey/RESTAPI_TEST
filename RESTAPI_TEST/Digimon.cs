af523c8e6dc4113a123112fb58de678ebad20fae
lil-hummonkey <hector.mcewen@elev.ga.ntig.se>
2024-05-06 13:02:42 +0200

    LabbAPi

---
+++ RESTAPI_TEST/Digimon.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RESTAPI_TEST;
+
+public class Digimon
+{
+public string name { get; set; }
+public int id { get; set; }
+public bool xAntibody { get; set; }
+}
+
+
---
+++ RESTAPI_TEST/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using System.IO;
+using RestSharp;
+using RESTAPI_TEST;
+
+RestClient client = new RestClient("https://digimon-api.com/api/v1/");
+Console.WriteLine("Pick a digimon to get information about it");
+string digiName = Console.ReadLine();
+RestRequest request = new($"digimon/{digiName}");
+
+try
+{
+
+    RestResponse response = client.GetAsync(request).Result;
+    Digimon d = JsonSerializer.Deserialize<Digimon>(response.Content);
+    
+    Console.WriteLine(d.name);
+    Console.WriteLine($"xAntibody:{d.xAntibody}");
+
+
+}
+catch
+{
+    System.Console.WriteLine("You aint funny bro");
+}
+
+
+
+// Console.WriteLine(response.Content);
+// File.WriteAllText("digimon.json", response.Content);
+
+Console.ReadLine();
+
